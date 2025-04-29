fe1e636489db7e5ba6b2f329c4dc6fa38c641fac
asma <asmaalnounou@gmail.com>
2025-04-29 19:39:28 +0300

    fix2

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// Enable CORS for all origins (development use)
+app.use(cors({ origin: '*' }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -15,7 +17,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: 'masarrah',
+  database: 'masarrah'
 });
 
 db.connect(err => {
@@ -25,13 +27,13 @@ db.connect(err => {
 
 // Route to handle form submission
 app.post('/contact', (req, res) => {
-  const { name, gender, mobile, dob, email, language, message } = req.body;
+  const { name, email, message, gender, mobile, dob, language } = req.body;
   const sql = `
-    INSERT INTO contact_messages (name, gender, mobile, dob, email, language, message)
+    INSERT INTO contact_messages (name, email, message, gender, mobile, dob, language)
     VALUES (?, ?, ?, ?, ?, ?, ?)
   `;
   
-  db.query(sql, [name, gender, mobile, dob, email, language, message], (err, result) => {
+  db.query(sql, [name, email, message, gender, mobile, dob, language], (err, result) => {
     if (err) {
       console.error('Error inserting data:', err);
       res.status(500).send('Server error');
